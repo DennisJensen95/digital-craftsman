981a878e08b96a32168ec133568fc0c3f68e98aa
Dennis Jensen <ngh1adj95@hotmail.com>
2023-08-01 15:06:41 +0200

    Feature: Added in rust app backend and only deploy from main
    Changes:
    - Deploy from main
    - Rust app

---
+++ apps/backend/backend-server/target/debug/deps/backend_server-3a7a5057fab39204.d
@@ -0,0 +1,5 @@
+/Users/dennisjensen/GIT/digital-craftsman/apps/backend/backend-server/target/debug/deps/backend_server-3a7a5057fab39204.rmeta: src/main.rs
+
+/Users/dennisjensen/GIT/digital-craftsman/apps/backend/backend-server/target/debug/deps/backend_server-3a7a5057fab39204.d: src/main.rs
+
+src/main.rs:
